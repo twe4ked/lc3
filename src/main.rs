8e93f339f215e779b2258d39e5ad9e585bfda855
Odin Dutton <odindutton@gmail.com>
2018-12-23 08:51:20 +1100

    Disable input buffering

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::process;
 use lc3;
 use lc3::Config;
+use termios::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,10 +10,35 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+    let termios = Termios::from_fd(0).unwrap_or_else(|err| {
+        println!("An error occured: {}", err);
+        process::exit(1);
+    });
+
+    disable_input_buffering(termios.clone());
 
     if let Err(e) = lc3::run(config) {
-        println!("Application error: {}", e);
+        enable_input_buffering(termios);
 
+        println!("Application error: {}", e);
         process::exit(1);
     }
+
+    enable_input_buffering(termios);
+}
+
+fn disable_input_buffering(mut termios: Termios) {
+    termios.c_lflag &= !(ICANON | ECHO);
+
+    tcsetattr(0, TCSANOW, &mut termios).unwrap_or_else(|err| {
+        println!("An error occured: {}", err);
+        process::exit(1);
+    });
+}
+
+fn enable_input_buffering(mut termios: Termios) {
+    tcsetattr(0, TCSANOW, &mut termios).unwrap_or_else(|err| {
+        println!("An error occured: {}", err);
+        process::exit(1);
+    });
 }
