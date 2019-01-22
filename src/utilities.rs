4cc272c9e4f3dbc02c01638bf17a3d87f76e27ae
Odin Dutton <odindutton@gmail.com>
2019-01-22 18:59:16 +1100

    Add tests for sign_extend()

--- src/utilities.rs
+++ src/utilities.rs
@@ -4,3 +4,18 @@ pub(crate) fn sign_extend(mut value: u16, bit_count: u8) -> u16 {
     }
     value
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sign_extend_positive_number() {
+        assert_eq!(sign_extend(0b01010, 5), 0b0000_0000_0000_1010);
+    }
+
+    #[test]
+    fn sign_extend_negative_number() {
+        assert_eq!(sign_extend(0b10101, 5), 0b1111_1111_1111_0101);
+    }
+}
