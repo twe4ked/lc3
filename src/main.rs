55d42c42603fcfbc65500f7ebada63e3402ea5f4
Odin Dutton <odindutton@gmail.com>
2019-08-23 13:22:47 +1000

    Simplify termios config

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,22 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    let termios = Termios::from_fd(libc::STDIN_FILENO).unwrap_or_else(|err| {
-        println!("An error occured: {}", err);
-        process::exit(1);
-    });
 
-    disable_input_buffering(termios);
+    disable_input_buffering();
 
     if let Err(e) = lc3::run(config) {
-        enable_input_buffering(termios);
-
         println!("Application error: {}", e);
         process::exit(1);
     }
-
-    enable_input_buffering(termios);
 }
 
-fn disable_input_buffering(mut termios: Termios) {
-    termios.c_lflag &= !(ICANON | ECHO);
-
-    tcsetattr(0, TCSANOW, &termios).unwrap_or_else(|err| {
+fn disable_input_buffering() {
+    let mut termios = Termios::from_fd(libc::STDIN_FILENO).unwrap_or_else(|err| {
         println!("An error occured: {}", err);
         process::exit(1);
     });
-}
+    termios.c_lflag &= !(ICANON | ECHO);
 
-fn enable_input_buffering(termios: Termios) {
     tcsetattr(0, TCSANOW, &termios).unwrap_or_else(|err| {
         println!("An error occured: {}", err);
         process::exit(1);
