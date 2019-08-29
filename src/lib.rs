b187094ae7c1b01e56fe50c61907d6355ea0389c
Odin Dutton <odindutton@gmail.com>
2019-08-29 18:34:22 +1000

    Rename file_loader to file

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 mod cpu;
 mod debugger;
-mod file_loader;
+mod file;
 mod instruction;
 mod sign_extend;
 mod state;
 
-use crate::{file_loader::read_rom, sign_extend::SignExtend, state::State};
+use crate::{sign_extend::SignExtend, state::State};
 use std::error::Error;
 
 pub fn run(filename: String, debug: bool) -> Result<(), Box<dyn Error>> {
-    let mut rom = read_rom(filename)?;
+    let mut rom = file::read_rom(filename)?;
     let mut state = State::new();
     state.load_rom(&mut rom);
 
