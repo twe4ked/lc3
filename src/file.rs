1d15859fcaa4d73958424e7c0df9c606825e6ff0
Odin Dutton <odindutton@gmail.com>
2019-09-01 14:05:28 +1000

    Remove byteorder dependency

--- src/file.rs
+++ src/file.rs
@@ -1,15 +1,37 @@
-use byteorder::{BigEndian, ByteOrder};
 use std::fs::File;
-use std::io::{Error, Read};
+use std::io::{Error, ErrorKind, Read};
 
 pub fn read_rom(filename: String) -> Result<Vec<u16>, Error> {
     let mut data = Vec::new();
     File::open(filename)?.read_to_end(&mut data)?;
+    from_bytes(&data)
+}
 
-    assert!(data.len() % 2 == 0, "invalid ROM");
+fn from_bytes(data: &[u8]) -> Result<Vec<u16>, Error> {
+    if data.len() % 2 != 0 {
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "input must be a multiple of 2",
+        ));
+    }
 
-    let mut buffer = vec![0; data.len() / 2];
-    BigEndian::read_u16_into(&data, &mut buffer);
+    Ok(data
+        .chunks(2)
+        .map(|x| x[1] as u16 | (x[0] as u16) << 8)
+        .collect())
+}
 
-    Ok(buffer)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_bytes() {
+        let data = from_bytes(&vec![0xaa, 0xbb, 0xcc, 0xdd]).unwrap();
+        assert_eq!(data, vec![0xaabb, 0xccdd]);
+
+        let result = from_bytes(&vec![0xaa, 0xbb, 0xcc]).map_err(|e| e.kind());
+        let expected = Err(ErrorKind::InvalidData);
+        assert_eq!(result, expected);
+    }
 }
